c2d46e47160d8a652376899a7c93d4285da827ce
yeong-jz <yeong.jz@gmail.com>
2018-03-27 00:44:37 -0700

    dfs

--- dbconfig/config.php
+++ dbconfig/config.php
@@ -1,5 +1,14 @@
 <?php
 /*For My LocalPC*/
-$con=mysqli_connect ("localhost", "root", "");
-mysqli_select_db ($con,'fyp2');
+// 1. Create a database connection
+$con=mysqli_connect("localhost", "root", "");
+if (!$con) {
+    die("Database connection failed: " . mysqli_error());
+}
+
+// 2. Select a database to use
+$db_select = mysqli_select_db ($con,'fyp2');
+if (!$db_select) {
+    die("Database selection failed: " . mysqli_error());
+}
 ?>
