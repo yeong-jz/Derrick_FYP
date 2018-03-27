3a32f8de389386bc1d4ebfcf73db3f731f35f7a8
yeong-jz <yeong.jz@gmail.com>
2018-03-27 00:49:09 -0700

    dfs

--- dbconfig/config.php
+++ dbconfig/config.php
@@ -7,7 +7,7 @@ if (!$con) {
 }
 
 // 2. Select a database to use
-$db_select = mysqli_select_db ($con,'fyp2');
+$db_select = mysqli_select_db($con,'fyp2');
 if (!$db_select) {
     die("Database selection failed: " . mysqli_error());
 }
