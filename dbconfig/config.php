6648bef0f8c0da92070569e46846606ae4410529
yeong-jz <yeong.jz@gmail.com>
2018-03-29 15:33:25 -0700

    edit config

--- dbconfig/config.php
+++ dbconfig/config.php
@@ -1,5 +1,5 @@
 <?php
 /*For My LocalPC*/
-$con=mysqli_connect ("172.20.113.8:3306", "root", "H7b67tRls8O0cnNF") or die ('I cannot connect to the database because: ' . mysql_error());
-mysqli_select_db ($con,'fyp2');
+$con=mysqli_connect ("172.20.113.8:3306", "", "H7b67tRls8O0cnNF", "fyp2") or die ('I cannot connect to the database because: ' . mysql_error());
+mysqli_select_db($con,'fyp2');
 ?>
